c2c6d7349d5b213ccb9bbf04c106224d552164bb
Kumar Abhirup <kumar@iqubex.com>
2019-03-03 00:14:02 +0530

    🐛 FIX: Schema issue Error: Expected Iterable, but did not find one for field Mutation. signIn.

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -3,12 +3,16 @@ const { isTokenValid } = require('../utils')
 
 async function signIn(parent, args, context){
 
+  const data = { ...args }
+  delete data.accessToken
+
   const isValid = await isTokenValid(args.signUpMethod, args.accessToken)
   if(!isValid) {
     throw new Error("Failed to recognize you!")
   }
 
-  throw new Error("SUCCESS")
+  const user = await context.prisma.createUser({...data})
+  return user
 
 }
 
