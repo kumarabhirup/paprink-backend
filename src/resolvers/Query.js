96cd231119284ef9d553ab78091c87d5dcb33aa8
Kumar Abhirup <kumar@iqubex.com>
2019-03-28 10:46:46 +0530

    📦 NEW: use context.db.mutation/query instead of context.prisma (#2)
    * ❎ SAVE: Use Prisma from prisma-binding and not the generated one
    The earlier structure caused some problems. I hope this won't. Also, need to change all queries and mutations to make things work.
    
    * 👌 IMPROVE: Change all queries and mutation calls to db
    
    * 📦 NEW: Stop using Prisma Client by prisma.
    Use Prisma offered by Prisma Binding. This is just what wesbos uses. It fixes the connection bugs, unleash the power of graphQL!

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,4 +1,5 @@
 const { forwardTo } = require('prisma-binding')
+const { postInfo } = require('../utils')
 
 async function users(parent, args, context, info) {
   throw new Error(`Sorry. But just go and fuck yourself!`)
@@ -7,7 +8,7 @@ async function users(parent, args, context, info) {
 async function me(parent, args, context, info){
   if(!context.request.userId){
     return null
-  } return context.prisma.user({id: context.request.userId})
+  } return context.db.query.user({where: {id: context.request.userId}}, info)
 }
 
 async function canUpdatePost(parent, args, context, info){
@@ -16,26 +17,12 @@ async function canUpdatePost(parent, args, context, info){
     throw new Error('Please SignIn to continue.')
   }
 
-  const postToUpdate = await context.prisma.post({id: args.id})
+  const postToUpdate = await context.db.query.post({where: {id: args.id}}, postInfo)
 
-  const canUpdate = postToUpdate.authorId === context.request.userId
-
-  const getPostAuthor = await context.prisma.user({ id: postToUpdate.authorId })
-  delete getPostAuthor.accessToken
-  delete getPostAuthor.socialId
-  delete getPostAuthor.updatedAt
-  delete getPostAuthor.createdAt
-  delete getPostAuthor.phone
-  delete getPostAuthor.birthday
-  delete getPostAuthor.bio
+  const canUpdate = postToUpdate.author.id === context.request.userId
 
   if (canUpdate) {
-    return {
-      ...postToUpdate,
-      author: {
-        ...getPostAuthor
-      }
-    }
+    return postToUpdate
   }
 
   throw new Error('You are not allowed to update this post.')
@@ -50,31 +37,18 @@ async function getPost(parent, args, context, info){
   const hasSlashAtLast = arrayOfUnfilteredPostId[arrayOfUnfilteredPostId.length - 1] === "/"
 
   const postId = hasSlashAtLast ? unfilteredPostId.slice(0, -1) : unfilteredPostId // is '252352532 (without slash)'
-  
-  const post = await context.prisma.post({id: postId})
-  const postAuthor = await context.prisma.user({ id: post.authorId })
-  delete postAuthor.accessToken
-  delete postAuthor.socialId
-  delete postAuthor.updatedAt
-  delete postAuthor.createdAt
-  delete postAuthor.phone
-  delete postAuthor.birthday
-  delete postAuthor.bio
+
+  const post = await context.db.query.post({where: {id: postId}}, postInfo)
 
   if (post) {
-    return {
-      ...post,
-      author: {
-        ...postAuthor
-      }
-    }
+    return post
   }
 
   throw new Error('POST NOT FOUND.')
 
 }
 
-var postsConnection = (parent, args, context, info) => forwardTo("prisma")(parent, args, context, info)
+var postsConnection = (parent, args, context, info) => forwardTo("db")(parent, args, context, info)
 
 module.exports = {
   users,
