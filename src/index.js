5d89e39270ae272c120641c153da550f423415d6
Kumar Abhirup <kumar@iqubex.com>
2019-05-30 20:33:19 +0530

    👌 IMPROVE: fix Co-authored-by: Harshit Pant <pantharshit00@gmail.com>

--- src/index.js
+++ src/index.js
@@ -3,10 +3,18 @@ require('dotenv').config()
 const { GraphQLServer } = require('graphql-yoga')
 const cookieParser = require('cookie-parser')
 const jwt = require('jsonwebtoken')
+const cors = require('cors')
 
 const Query = require('./resolvers/Query')
 const Mutation = require('./resolvers/Mutation')
 const db = require('./db')
+
+const corsMW = {
+  credentials: true,
+  origin: process.env.NODE_ENV === 'development'
+          ? process.env.FRONTEND_URL
+          : process.env.PROD_FRONTEND_URL,
+};
 
 // start it
 const server = new GraphQLServer({
@@ -28,6 +36,9 @@ const server = new GraphQLServer({
 
 // COOKIE PARSER
 server.express.use(cookieParser())
+
+server.express.use(cors(corsMW));
+server.express.options('*', cors(corsMW));
 
 // Decode the JWT
 server.express.use((req, res, next) => {
@@ -57,15 +68,11 @@ server.express.use(async (req, res, next) => {
   next()
 })
 
+// got it :P
+
 server.start(
   {
-    cors: {
-      credentials: true,
-      origin:
-        process.env.NODE_ENV === 'development'
-          ? process.env.FRONTEND_URL
-          : process.env.PROD_FRONTEND_URL,
-    },
+    cors: corsMW,
     endpoint: '/graphql',
     // playground: process.env.NODE_ENV === 'development' ? '*' : false
   },
