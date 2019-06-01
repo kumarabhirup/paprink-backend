041647fa69a27be47cf5041a7b82f9db73971296
Kumar Abhirup <kumar@iqubex.com>
2019-06-01 20:12:09 +0530

    📦 NEW: No Lag on Frontend! (#8)
    * ❎ SAVE: console log for debugging
    
    * 👌 IMPROVE: Uniform schema
    
    * 📖 DOC: Change text
    
    * ❎ SAVE: Temp chnages to revert
    
    * 👌 IMPROVE: Slight change
    
    * 🐛 FIX: fix duplicate graphql dep and update prisma (#7)
    
    * 👌 IMPROVE: lock
    
    * 👌 IMPROVE: remove console logs
    
    * 👌 IMPROVE: fix
    Co-authored-by: Harshit Pant <pantharshit00@gmail.com>
    
    * 👌 IMPROVE: Small, but useless changes
    
    * 👌 IMPROVE: change endpoint
    
    * 👌 IMPROVE: Set debug to false
    
    * 👌 IMPROVE: Delete .env
    
    * 👌 IMPROVE: Delete .env

--- src/index.js
+++ src/index.js
@@ -3,66 +3,75 @@ require('dotenv').config()
 const { GraphQLServer } = require('graphql-yoga')
 const cookieParser = require('cookie-parser')
 const jwt = require('jsonwebtoken')
+const cors = require('cors')
 
 const Query = require('./resolvers/Query')
 const Mutation = require('./resolvers/Mutation')
 const db = require('./db')
+
+const corsMW = {
+  credentials: true,
+  origin: process.env.NODE_ENV === 'development'
+          ? process.env.FRONTEND_URL
+          : process.env.PROD_FRONTEND_URL,
+}
 
 // start it
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers: {
     Query,
-    Mutation
+    Mutation,
   },
   resolverValidationOptions: {
     requireResolversForResolveType: false,
   },
-  context: request => ({
-    ...request,
-    db
-  }),
+  context: request => {
+    return {
+      ...request,
+      db,
+    }
+  },
 })
 
 // COOKIE PARSER
 server.express.use(cookieParser())
 
+server.express.use(cors(corsMW))
+server.express.options('*', cors(corsMW))
+
 // Decode the JWT
 server.express.use((req, res, next) => {
-    const { paprinkToken } = req.cookies
-    if(paprinkToken){
-      const { userId } = jwt.verify(paprinkToken, process.env.JWT_SECRET)
-      if(userId) {
-        req.userId = userId
-      }
+  const { paprinkToken } = req.cookies
+  if (paprinkToken) {
+    const { userId } = jwt.verify(paprinkToken, process.env.JWT_SECRET)
+    if (userId) {
+      req.userId = userId
     }
-    next()
+  }
+  next()
 })
 
 // Populate the user
 server.express.use(async (req, res, next) => {
+  // skip if they aren't logged in
+  if (!req.userId) {
+    return next()
+  }
 
-    // skip if they aren't logged in
-    if(!req.userId){
-        return next()
-    }
+  const user = await db.query.user(
+    { where: { id: req.userId } },
+    '{ id, fname, lname, name, email, previledge }',
+  )
+  req.user = user
 
-    const user = await db.query.user({ where: {id: req.userId} }, '{ id, fname, lname, name, email, previledge }')
-    req.user = user
-
-    next()
-
+  next()
 })
-
 
 server.start(
   {
-    cors: {
-      credentials: true,
-      origin: process.env.NODE_ENV === 'development' ? process.env.FRONTEND_URL : process.env.PROD_FRONTEND_URL
-    },
-    endpoint: '/graphql',
-    playground: process.env.NODE_ENV === 'development' ? '*' : false
+    cors: corsMW,
+    endpoint: '/api/graphql'
   },
-  details => console.log(`Server is running on PORT ${details.port}`)
+  details => console.log(`Server is running on PORT ${details.port}`),
 )
